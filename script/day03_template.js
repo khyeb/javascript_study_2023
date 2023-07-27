b1acf92f6e58ea297dee2be06b9bfbee95a9372c
hyebin <bin930827@gmail.com>
2023-07-27 14:31:46 +0900

    javascript0727

---
+++ script/day03_template.js
@@ -0,0 +1,15 @@
+//자바스크립트=객체지향언어
+//객체구분>변수생성>변수활동속성,이벤트,메소드
+//--------------------------------------getElement 객체방법
+//--------------------------------------querySelector 객체방법
+//const aTagQ1=doqument.querySelectorAll('#wrap a:nth:-child(1))
+//--------------------------------------변수정상출력확인
+console.log();
+//--------------------------------------
+console.log('---------------------------------------')
+console.log('li태그 변수값은'+liTagQ[0]+'입니다.')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+console.log('')
